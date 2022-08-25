a8111531ebfb0a76aad7a028171702dc81697c28
CamilaSilva <camiladasilvaassis1@gmail.com>
2022-08-25 19:07:18 -0300

    feature: criação do projeto

---
+++ buscarcep/src/App.js
@@ -0,0 +1,21 @@
+// Criado por Camila Silva
+// Desde 25/08/2022
+// Este script é responsavel em renderizar os componentes
+
+function App() {
+  return (
+    <div id="container">
+
+        <h1 className="title">Buscar Cep</h1>
+
+        <div className="container-input">
+
+            <input type="text" placeholder="Digite o cep"/>
+
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
