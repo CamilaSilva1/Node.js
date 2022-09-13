7822ae740f6c91dd61b9a21e9dc25d1f4b818d07
CamilaSilva <camiladasilvaassis1@gmail.com>
2022-09-13 15:08:46 -0300

    feature: inserindo componentes

--- buscarcep/src/App.js
+++ buscarcep/src/App.js
@@ -2,72 +2,23 @@
 // Desde 25/08/2022
 // Este script é responsavel em renderizar os componentes
 
-import {useState} from 'react';
-import {FiSearch} from 'react-icons/fi';
-import style from './styles/style.css';
-import api from './services/api';
+import React from 'react';
 
-function App() {
+import Container from './components/Container';
 
-  const [input, setInput] = useState('');
-  const [cep, setCep] = useState({});
+import styles from './modules/Container.module.css'; 
 
-  async function handleSearch(){
+class App extends React.Component{
 
-      if(input === ''){
-        alert("É preciso digitar o cep!");
-        return;
-      }
+  render(){
+    return (
+      <div id={styles.container}>
+  
+          <Container />
 
-      try{
-
-        const response = await api.get(`${input}/json`);
-        console.log(response.data);
-        setCep(response.data);
-        setInput("");
-        
-      }catch{
-
-        alert("Erro ao buscar CEP");
-        setInput("");
-
-      }
+      </div>
+    );
   }
-
-  return (
-    <div id="container">
-
-        <h1 className="title">Buscar Cep</h1>
-
-        <div className="container-input">
-
-            <input type="text" 
-                   placeholder="Digite o cep"
-                   value={input}
-                   onChange={ (event) => setInput(event.target.value) }/>
-                
-            <button className="btn-search" onClick={handleSearch}> <FiSearch/> </button>
-
-        </div>
-
-       
-        {Object.keys(cep).length > 0 && (
-
-              <main className="main-content">
-              
-                <h2> CEP: {cep.cep} </h2>
-                
-                <span>Rua: {cep.logradouro} </span>
-                <span> Complemento: {cep.complemento} </span>
-                <span> Bairro: {cep.bairro} </span>
-                <span> {cep.localidade} - {cep.uf} </span>
-
-              </main>
-
-        )}
-           
-    </div>
-  );
 }
 
 export default App;
