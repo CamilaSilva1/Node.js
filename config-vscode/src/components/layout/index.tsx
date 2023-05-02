edbe7985155b963bfa960bbf0d98e361fd00c4cf
camila <camila.assis@smarketsolutions.com.br>
2023-05-02 19:12:20 -0300

    wip

---
+++ config-vscode/src/components/layout/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles"
+
+const Layout = (): JSX.Element => {
+    return (
+       <>
+            <Container>
+                <h1>Teste</h1>
+            </Container>
+       </>
+    )
+}
+
+export default Layout
---
+++ config-vscode/src/components/layout/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    width: 1100px;
+    height: 540px;
+    background: #34283c;
+`
