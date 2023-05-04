bbf6ac3947da07a11aac5fbf47cc8b992d514213
camila <camila.assis@smarketsolutions.com.br>
2023-05-04 19:23:48 -0300

    wip

--- config-vscode/src/components/layout/index.tsx
+++ config-vscode/src/components/layout/index.tsx
@@ -1,10 +1,35 @@
-import { Container } from "./styles"
+import { Backpack, Bug, Files, GitFork, Microscope, Monitor, Search, Settings, User } from "lucide-react"
+import { BorderLeftSideBar, Circles, Container, Footer, SideBar, SideBarBottom } from "./styles"
 
 const Layout = (): JSX.Element => {
     return (
        <>
             <Container>
-                <h1>Teste</h1>
+                <Circles background="red"/>
+                <Circles background="yellow" />
+                <Circles background="green" />
+
+                <BorderLeftSideBar />
+                    
+                <SideBar>
+                    <Files color="#d2d2d2"/>
+                    <Search color="#d2d2d2"/>
+                    <Backpack color="#d2d2d2"/>
+                    <Bug color="#d2d2d2"/>
+                    <GitFork color="#d2d2d2"/>
+                    <Monitor color="#d2d2d2"/>
+                    <Microscope color="#d2d2d2"/>
+
+                    <SideBarBottom>
+                        <User color="#d2d2d2"/>
+                        <Settings color="#d2d2d2"/>
+                    </SideBarBottom>
+
+                </SideBar>
+
+                <Footer>
+                    <span>main</span>
+                </Footer>
             </Container>
        </>
     )
--- config-vscode/src/components/layout/styles.ts
+++ config-vscode/src/components/layout/styles.ts
@@ -1,8 +1,55 @@
 import styled from 'styled-components'
 
+
+type Props = {
+    background: string;
+}
+
 export const Container = styled.div`
     display: flex;
     width: 1100px;
     height: 540px;
     background: #34283c;
+    gap: 5px;
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 3px 3px 3px 2px rgba(0, 0, 0, 0.6);
+`
+export const Circles = styled.div`
+    background: ${({ background }: Props): string => background};
+    border-radius: 40px;
+    width: 12px;
+    height: 12px;
+    cursor: pointer;
+`
+
+export const SideBar = styled.div`
+    display: flex;
+    position: fixed;
+    flex-direction: column;
+    left: 100px;
+    margin-top: 30px;
+    gap: 10px;
+    cursor: pointer;
+`
+
+export const SideBarBottom = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    bottom: 70px;
+    gap: 15px;
+`
+
+export const Footer = styled.span`
+    position: fixed;
+    bottom: 40px;
+`
+
+export const BorderLeftSideBar = styled.div`
+    position: absolute;
+    left: 87px;
+    top: 70px;
+    height: 40px;
+    border-left: 2px solid pink;
 `
